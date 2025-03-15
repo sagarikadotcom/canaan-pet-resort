3c7ab2946188134f10d702581ee1d6e6c9dc88ca
sagarika.katika <sagarika.katikam@gmail.com>
2025-03-15 14:08:50 +0530

    __dir name

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,22 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { fileURLToPath } from "url";
 import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+// Manually define __dirname in an ES Module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
 export default [
-  // Extends
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-
-  // Custom rules
-  {
+  ...compat.config({
+    extends: ["next/core-web-vitals"],
     rules: {
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "react/jsx-no-duplicate-props": "off",
     },
-  },
+  }),
 ];
